6268b261136d1e0230a2c5f31a2d7781222a0568
Andre Aguillard <AguillardDre@gmail.com>
2017-04-28 10:35:20 -0500

    Add files via upload

---
+++ Final Project.py
@@ -0,0 +1,14 @@
+## Python Code for CYEN 132 Final Rpi Assignement
+from tkinter import *
+from Rpi import GPIO
+
+
+
+# GUI to display picture, letters left, and letter input.
+e1 = 
+
+
+# GPIO pins that light up with wrong, incorrect, and 
+
+
+
