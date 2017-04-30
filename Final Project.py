5a2124c289c30362fcb4f97daf5da789473d4c82
Andre Aguillard <AguillardDre@gmail.com>
2017-04-30 17:22:50 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,18 +1,37 @@
 ## Python Code for CYEN 132 Final Rpi Assignement
-from tkinter import *
+from Tkinter import *
 import RPi.GPIO as GPIO
 
 NumberOfLives = 6
+HangManword = "Word"
 
 # GUI to display picture, letters left, and letter input.
 class GUI4Hangman(Frame):
-  def __init__(self, master):
-    Frame.__init__(self,master) ### This sets up the main window of the GUI
-    self.master = master     ### in order to build widgets on to top it.
+  def __init__(self, parent):
+    # calls constructor in superclass
+    Frame.__init__(self,parent) ### This sets up the main window of the GUI
+    self.parent = parent        ### in order to build widgets on to top it.
+                
+  def setupGUI(self): ##Initializes the layout for the GUI
+    #Organize the GUI
+    # this function works fine, as long as you have the images as actual GIFs
+    self.pack(fill=BOTH, expand=1)
+    #setup the player input at the bottom of the GUI
+    #widget is a Tkinter Entry
+    #background is white; bind return key to function process in class
+    GUI4Hangman.player_input = Entry(self, bg="white")
+    GUI4Hangman.player_input.bind("<Return>", self.process)
+    GUI4Hangman.player_input.pack(side=BOTTOM, fill=X)
+    GUI4Hangman.player_input.focus()
     
-  def setupGUI(self): ##Initializes the layout for the Gui
-    e1 = Entry  (self.master)
-    e1.pack(side=LEFT, expand=1)
+    # setup image to the left of GUI
+    # widget is a Tkinter label
+    # don't let image control width's size
+    img = None
+    GUI4Hangman.image = Label(self, width=WIDTH / 2, height=HEIGHT, image = img)
+    GUI4Hangman.image.image = img
+    GUI4Hangman.image.pack(side=LEFT, fill=Y)
+    GUI4Hangman.image.pack_propagate(False)
 
 
 # GPIO pins that light up with wrong, incorrect, and number of lives left
