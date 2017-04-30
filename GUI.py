99c21899ae56c5ac7d1a6d468f6145715cf12f03
Andre Aguillard <AguillardDre@gmail.com>
2017-04-30 18:17:12 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,18 +1,72 @@
-from tkinter import *
+## Python Code for CYEN 132 Final Rpi Assignement
+from Tkinter import *
+
+NumberOfLives = 6
+HangManword = "word"
 
 # GUI to display picture, letters left, and letter input.
-class GUI4Hangman(Frame):
-  def __init__(self, master):
-    Frame.__init__(self,master) ### This sets up the main window of the GUI
-    self.master = master     ### in order to build widgets on to top it.
+class Hangman(Frame):
+  def __init__(self, parent):
+    # calls constructor in superclass
+    Frame.__init__(self,parent) ### This sets up the main window of the GUI
+    self.parent = parent        ### in order to build widgets on to top it.
+                
+  def setupGUI(self): ##Initializes the layout for the GUI
+    #Organize the GUI
+    # this function works fine, as long as you have the images as actual GIFs
+    self.pack(fill=BOTH, expand=1)
+    #setup the player input at the bottom of the GUI
+    #widget is a Tkinter Entry
+    #background is white; bind return key to function process in class
+    Hangman.player_input = Entry(self, bg="white")
+    Hangman.player_input.bind("<Return>", self.process)
+    Hangman.player_input.pack(side=BOTTOM, fill=X)
+    Hangman.player_input.focus()
     
-  def setupGUI(self): ##Initializes the layout for the Gui
-    e1 = Entry  (self.master)
-    e1.pack(side=LEFT, expand=1)
-
-
+    # setup image to the left of GUI
+    # widget is a Tkinter label
+    # don't let image control width's size
+    img = None
+    Hangman.image = Label(self, width=WIDTH / 2, height=HEIGHT, image = img)
+    Hangman.image.image = img
+    Hangman.image.pack(side=LEFT, fill=Y)
+    Hangman.image.pack_propagate(False)
+    # first, place frame where the text will be displayed
+    text_frame = Frame(self, width=WIDTH / 2, height=HEIGHT)
+    # widget - same deal as above
+    # disable by default
+    # don't let it control frame's size
+    Hangman.text = Text(text_frame, bg="lightgrey", state=DISABLED)
+    Hangman.text.pack(fill=Y, expand=1)
+    text_frame.pack(side=RIGHT, fill=Y)
+    text_frame.pack_propagate(False)
+    
+  def setHangmanImage(self):
+    Hangman.img = PhotoImage(file="skull.gif")
+    Hangman.image.config(image=Hangman.img)
+    Hangman.image.image = Hangman.img
+    
+  def numberOfLives(self):
+    # Light up the number of LED's equal to the number of Lives the user has left.
+    pass
+# GPIO pins that light up with wrong, incorrect, and number of lives left
+  def play(self):
+    # configure GUI
+    self.setupGUI()
+    # set the Hangman Image
+    self.setHangmanImage()
+    
+  def process(self):
+    pass
+    
+# the default size of the GUI is 800x600
+WIDTH = 800
+HEIGHT = 600
+  
+# Main Code 
+#create the window
 window = Tk()
 window.title("Don't blow up the Death Star")
-t = GUI4Hangman(window)
-t.setupGUI()
+t = Hangman(window)
+t.play()
 window.mainloop()
