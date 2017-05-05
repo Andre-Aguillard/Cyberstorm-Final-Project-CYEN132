d443e1ed45a5f04c30b1788914f6a831e90ebcfd
Andre Aguillard <AguillardDre@gmail.com>
2017-05-05 10:16:20 -0500

    Updated the Final Code
    I'm gonna work on the GUI some now, please check out my changes.

--- Final Project.py
+++ Final Project.py
@@ -1,14 +1,31 @@
 # Python Code for CYEN 132 Final Rpi Assignement
+# This is the final piece of code we are going to submit. 
+# As I see features working in the other .py files, I'll add them here. -Andre
 from Tkinter import *
 import RPi.GPIO as GPIO
 
 NumberOfLives = 6
-HangmanWord = "word"
+HangmanWord = "Andromeda Antlia Apus Aquarius Aquila Ara Aries Auriga Bootes Caelum \
+        Camelopardalis Cancer Canes_Venatici Canis_Major Canis_Minor \
+        Capricornus Carina Cassiopeia Centaurus Cepheus Cetus Chamaeleon \
+        Circinus Columba Coma_Berenices Corona_Austrina Corona_Borealis \
+        Corvus Crater Crux Cygnus Delphinus Dorado Draco Equuleus Eridanus \
+        Fornax Gemini Grus Hercules Horologium Hydra Hydrus Indus Lacerta Leo \
+        Leo_Minor Lepus Libra Lupus Lynx Lyra Mensa Microscopium Monoceros \
+        Musca Norma Octans Ophiuchus Orion Pavo Pegasus Perseus Phoenix \
+        Pictor Pisces Piscis_Austrinus Puppis Pyxis Reticulum Sagitta \
+        Sagittarius Scorpius Sculptor Scutum Serpens Sextans Taurus \
+        Telescopium Triangulum Triangulum_Australe Tucana Ursa_Major \
+        Ursa_Minor Vela Virgo Volans Vulpecula".split()"
 
-class Images(object):
-    def __init__(self, name):
-        self.name = name
-        self.images = [] # list for images
+# GUI to display picture, letters left, and letter input.
+class Hangman(Frame):
+
+    def __init__(self, parent):
+        # calls constructor in superclass
+        Frame.__init__(self, parent)  # This sets up the main window of the GUI
+        self.parent = parent          # in order to build widgets on to top it.
+        self.images =[]
         
     # image getter   
     @property
@@ -21,15 +38,6 @@ class Images(object):
     ## Add image function to easily change the image
     def addImage (self, image):
         self._images.append(image)
-
-# GUI to display picture, letters left, and letter input.
-class Hangman(Frame):
-
-    def __init__(self, parent):
-        # calls constructor in superclass
-        Frame.__init__(self, parent)  # This sets up the main window of the GUI
-        self.parent = parent          # in order to build widgets on to top it.
-
         
     def setupGUI(self):  # Initializes the layout for the GUI
         # Organize the GUI
@@ -73,31 +81,48 @@ class Hangman(Frame):
         # has left.
         pass
 
-    def setStatus(self, status):
+    def display(self, response):
         # enable text widget, clear it, set it, disable it
         Hangman.text.config(state=NORMAL)
         Hangman.text.delete("1.0", END)
-        Hangman.text.insert(
-            END, "Welsom to intergalactic hangman, may I take your order?")
+        Hangman.text.insert(END, "Welcome to intergalactic hangman, may I take your order?")
+        Hangman.text.config(state=DISABLED)
 
-# GPIO pins that light up with wrong, incorrect, and number of lives left
+# Need to add code for GPIO pins that light up with wrong, incorrect, and number of lives left
     def play(self):
         # configure GUI
         self.setupGUI()
         # set the Hangman Image
         self.setHangmanImage()
-        # setup the text box to the left of the GUI
-        self.setStatus
+        # setup the text to display in the box to the left of the GUI
+        self.display
 
     def process(self):
         pass
-        
-        self.setStatus(response)
+        # Grab the player's input from the input at the bottom of the GUI
+        action = Hangman.player_input.get()
+        # Convert all player input to lowercase.
+        action = action.lower()
+        # Make sure player input is only one character:
+        ## Splits the player input based off white spaces and stores that in a list
+        characters = action.split()
+        if len(characters > 1):
+            response = "Please input only one character as your guess. Thank you."
+            display(response)
+        else: 
+            if (action in currentWord): '''NOTE: currentWord should be set up to be the current word to guess ''' 
+                pass
+                #Do something here
+                #
+            else:
+                #IT not in word so blow up the death star some.
+                response = character not in word. 
+        self.display(response)
         self.setRoomImage()
         Hangman.player_input.delete(0, END)
 
 
-#################### MAIN FUNCTION that sets everything up ####################
+#################### MAIN Code that runs before functions above everything up ####################
 # Initialize the Raspberry Pi by disabeling any warnings and telling it
 # we're going to use the BCM pin numbering scheme.
 GPIO.setwarnings(False)
@@ -106,6 +131,7 @@ GPIO.setmode(GPIO.BCM)
 # No guesses have been made, so set both pins low to start
 GPIO.output(26, GPIO.LOW)
 GPIO.output(13, GPIO.LOW)
+
 # Initialize the number of lives LEDs here
 # All of these should start lit up
 pins = [21, 20, 26, 12, 19, 6, 5]  # list of GPIO pins for lights
